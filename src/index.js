e186096822e32fe1d823d954e035534c8dc0944c
Idan Tabachnik <idantb101@gmail.com>
2018-02-11 16:08:03 +0200

    creating a basic router relates to #17

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
+import { render } from 'react-dom';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+render(
+  <BrowserRouter>
+    <Route exact path="/" component={App} />
+  </BrowserRouter>,
+  document.getElementById('root')
+);
