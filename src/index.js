f27057c7d2bd09344eadb72ebf7ba7ec5536f8b0
Idan Tabachnik <idantb101@gmail.com>
2018-02-11 16:30:18 +0200

    deleting unnecessary comment related to #17

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { render } from 'react-dom';
-import ReactDOM from 'react-dom';
 import { BrowserRouter, Route } from 'react-router-dom';
 import './index.css';
 import App from './App';
