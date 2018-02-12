01a5083289ef605bc95397a5132142f26668836e
Idan Tabachnik <idantb101@gmail.com>
2018-02-12 16:02:08 +0200

    fixing the prak list page, creating a tags  component, creating a component for a single park as part of the list, relates to #14

---
+++ src/components/SingleParkFromList.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import GreenTag from './GreenTag';
+
+const SingleParkFromList = props => {
+  return (
+    <section className="park_container flex flex-column items-center z-0 w-100 flex-row-ns justify-center-ns">
+      <div className="first_park bg-light-gray w-80 h5 mt4 br2 w-40-ns vh-40-ns mr5-ns">
+        <p className="tc ma3 f3 b f2-l">{props.park.parkName}</p>
+        <GreenTag tags={props.park.tags} />
+        <figure className="image w-70 mt3 center">
+          <img src={props.park.picturesUrl.big} alt="park pic" />
+        </figure>
+      </div>
+    </section>
+  );
+};
+
+export default SingleParkFromList;
