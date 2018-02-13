820f8f386cd556d2c611f3e11ab91d85d93da144
Idan Tabachnik <idantb101@gmail.com>
2018-02-13 12:40:16 +0200

    deleting unnecessary files and creating logic for navbar component relates to #14

--- src/index.js
+++ src/index.js
@@ -1,15 +1,12 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { BrowserRouter, Route } from 'react-router-dom';
-import './index.css';
-import App from './App';
 import ParkList from './components/ParkList';
 
 render(
   <BrowserRouter>
     <div>
-      <Route exact path="/" component={App} />
-      <Route exact path="/parkList" component={ParkList} />
+      <Route exact path="/" component={ParkList} />
     </div>
   </BrowserRouter>,
   document.getElementById('root')
