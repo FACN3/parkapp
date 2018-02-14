b584022752b7a05f6cdfe2495e46e2796d691c90
sophieeejw <jungwon.lim.0210@gmail.com>
2018-02-14 15:45:21 +0200

    Update MyMapComponent.js

--- src/components/MyMapComponent.js
+++ src/components/MyMapComponent.js
@@ -1,6 +1,6 @@
 import { Map, InfoWindow, Marker, GoogleApiWrapper } from 'google-maps-react';
 import React, { Component } from 'react';
-//require('env2')('config.env');
+require('env2')('.env');
 
 export class MapContainer extends Component {
   render() {
