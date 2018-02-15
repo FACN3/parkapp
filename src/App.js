0be1703fcc99482700d472fe249aa0b9245d35bf
Idan Tabachnik <idantb101@gmail.com>
2018-02-15 14:58:24 +0200

    Creating a basic layout for Google API. adding current .env settings. adding location and markers. refers to #49

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar';
-import MyMapComponent from './components/MyMapComponent';
+import MapComponent from './components/MapComponent';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Navbar />
-        <MyMapComponent />
+        <MapComponent />
       </div>
     );
   }
