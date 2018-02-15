48667e627acf91d317b8ee7bd9b9eb9e701d395e
“Sophia” <jungwon.lim.0210@gmail.com>
2018-02-15 16:47:10 +0200

    created database on mlab and connected it to router Relates #35

--- server/db/db_connections.js
+++ server/db/db_connections.js
@@ -1,18 +1,19 @@
-require('env2')('.env');
-const dbConfig = process.env.mongodb_url;
-const mongoose = require('mongoose');
+const db_connection = () => {
+  require('env2')('.env');
+  const dbConfig = process.env.mongodb_url;
+  const mongoose = require('mongoose');
 
-mongoose.Promise = global.Promise;
+  mongoose.Promise = global.Promise;
 
-mongoose.connect(dbConfig.url, {
-    useMongoClient: true
-});
+  mongoose.connect(dbConfig);
 
-mongoose.connection.on('error', () => {
-console.log('Could not connect to the database. Exiting now');
-process.exit();
-});
+  mongoose.connection.on('error', () => {
+    console.log('Could not connect to the database. Exiting now');
+    process.exit();
+  });
 
-mongoose.connect.on('open', () => {
-    console.log('Successfully connected to the database')
-})
+  mongoose.connection.once('open', () => {
+    console.log('Successfully connected to the database');
+  });
+};
+module.exports = db_connection;
